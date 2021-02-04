718110d85c81e73309f7390a1f516c7be1ad9c37
AlexanderKovalenko <alexander.kovalenko@devexpress.com>
2021-02-04 21:55:16 +0300

    Initial commit

---
+++ DashboardLocalizationCore/Models/CustomDashboardStorage.cs
@@ -0,0 +1,54 @@
+﻿using DevExpress.DashboardCommon;
+using DevExpress.DashboardWeb;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+public class CustomDashboardStorage : IEditableDashboardStorage {
+    public string RootPath { get; set; }
+
+    public CustomDashboardStorage(string path) {
+        RootPath = path;
+    }
+
+    public IEnumerable<DashboardInfo> GetAvailableDashboardsInfo() {
+        var dashboardInfos = new List<DashboardInfo>();
+
+        var files = Directory.GetFiles(RootPath, "*.xml");
+
+        foreach (var item in files) {
+            var name = Path.GetFileNameWithoutExtension(item);
+
+            if (!name.StartsWith("_"))
+                dashboardInfos.Add(new DashboardInfo() { ID = name, Name = name });
+        }
+
+        return dashboardInfos;
+    }
+
+    public XDocument LoadDashboard(string dashboardID) {
+        var path = Path.Combine(RootPath, dashboardID + ".xml");
+        var content = File.ReadAllText(path);
+
+        return XDocument.Parse(content);
+    }
+
+    public string AddDashboard(XDocument dashboard, string dashboardName) {
+        /*var path = HttpContext.Current.Server.MapPath("~/App_Data/Dashboards/" + dashboardName + ".xml");
+
+        File.WriteAllText(path, dashboard.ToString());
+
+        return dashboardName;*/
+        return null;
+    }
+
+    public void SaveDashboard(string dashboardID, XDocument dashboard) {
+        /*var path = HttpContext.Current.Server.MapPath("~/App_Data/Dashboards/" + dashboardID + ".xml");
+
+        File.WriteAllText(path, dashboard.ToString());*/
+    }
+}
+
---
+++ DashboardLocalizationCore/Startup.cs
@@ -0,0 +1,71 @@
+using DevExpress.AspNetCore;
+using DevExpress.DashboardAspNetCore;
+using DevExpress.DashboardCommon;
+using DevExpress.DashboardWeb;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+
+namespace ASPNETCore30Dashboard {
+    public class Startup {
+        public Startup(IConfiguration configuration, IWebHostEnvironment hostingEnvironment) {
+            Configuration = configuration;
+            FileProvider = hostingEnvironment.ContentRootFileProvider;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IFileProvider FileProvider { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+            // Configures services to use the Web Dashboard Control.
+            services
+                .AddDevExpressControls()
+                .AddControllersWithViews()
+                .AddDefaultDashboardController(configurator => {
+                    configurator.SetDashboardStorage(new DashboardFileStorage(FileProvider.GetFileInfo("App_Data/Dashboards").PhysicalPath));
+
+                    DataSourceInMemoryStorage dataSourceStorage = new DataSourceInMemoryStorage();
+                    DashboardObjectDataSource objDataSource = new DashboardObjectDataSource("ObjectDataSource", typeof(ProductSales));
+                    objDataSource.DataMember = "GetProductSales";
+                    dataSourceStorage.RegisterDataSource("objectDataSource", objDataSource.SaveToXml());
+
+                    configurator.SetDataSourceStorage(dataSourceStorage);
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            var supportedCultures = new[] { "en-US", "de-DE" };
+            var opts = new RequestLocalizationOptions()
+                .SetDefaultCulture(supportedCultures[1])
+                .AddSupportedCultures(supportedCultures)
+                .AddSupportedUICultures(supportedCultures);
+            opts.RequestCultureProviders.Clear();
+            app.UseRequestLocalization(opts);
+
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            } else {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            // Registers the DevExpress middleware.
+            app.UseDevExpressControls();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => {
+                // Maps the dashboard route.
+                EndpointRouteBuilderExtension.MapDashboardRoute(endpoints, "api/dashboards");
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
