6e08a0f2586496b9db84785e36df5123163482e5
Polina Tyureva <polina.tyureva@devexpress.com>
2025-08-29 20:26:34 +0400

    update example

--- CS/DashboardLocalizationManualCore/Program.cs
+++ CS/DashboardLocalizationManualCore/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Logging;
 namespace ASPNETCore30Dashboard {
     public class Program {
         public static void Main(string[] args) {
+          DevExpress.Utils.DeserializationSettings.RegisterTrustedClass(typeof(ProductSales));
             CreateHostBuilder(args).Build().Run();
         }
 
